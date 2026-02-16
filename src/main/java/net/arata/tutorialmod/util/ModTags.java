37a9b72f362caf24efef1fdbc572836bc910fd91
jacobotron03 <jacobotron03@gmail.com>
2026-02-16 16:58:55 +0100

    Added first custom BlockTag - Valuables Ores for Metal Detector

---
+++ src/main/java/net/arata/tutorialmod/util/ModTags.java
@@ -0,0 +1,31 @@
+package net.arata.tutorialmod.util;
+
+import net.arata.tutorialmod.TutorialMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+// Las tags son etiquetas que se ponen a ciertos objetos para agruparlos o identificarlos
+public class ModTags {
+
+    public static class Blocks {
+
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+
+    }
+}
