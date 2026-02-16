2eaca134b51aa9d06891903c1b3a3ba183cb9efa
jacobotron03 <jacobotron03@gmail.com>
2026-02-16 15:10:20 +0100

    Added tooltips to Metal Detector Item and Sound Block

--- src/main/java/net/arata/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/arata/tutorialmod/item/custom/MetalDetectorItem.java
@@ -6,12 +6,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
 
@@ -52,6 +57,11 @@ public class MetalDetectorItem extends Item {
         return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.metal_detector"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
