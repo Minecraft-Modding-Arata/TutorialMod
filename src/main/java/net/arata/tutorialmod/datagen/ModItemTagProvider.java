d8426128b0f84e3c0c8632d8329e452318da36b2
jacobotron03 <jacobotron03@gmail.com>
2026-02-18 14:24:10 +0100

    Added trimmable Sapphire Armor Set

--- src/main/java/net/arata/tutorialmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/arata/tutorialmod/datagen/ModItemTagProvider.java
@@ -1,9 +1,11 @@
 package net.arata.tutorialmod.datagen;
 
 import net.arata.tutorialmod.TutorialMod;
+import net.arata.tutorialmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,7 @@ public class ModItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-
+        this.tag(ItemTags.TRIMMABLE_ARMOR).add(ModItems.SAPPHIRE_HELMET.get(), ModItems.SAPPHIRE_CHESTPLATE.get(),
+                ModItems.SAPPHIRE_LEGGINGS.get(), ModItems.SAPPHIRE_BOOTS.get());
     }
 }
