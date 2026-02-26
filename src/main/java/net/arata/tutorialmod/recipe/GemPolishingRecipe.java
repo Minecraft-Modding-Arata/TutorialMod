c026ea5832e67d97b3ed9838cbec1e1288afc031
jacobotron03 <jacobotron03@gmail.com>
2026-02-26 21:37:34 +0100

    Added JEI compatibility

--- src/main/java/net/arata/tutorialmod/recipe/GemPolishingRecipe.java
+++ src/main/java/net/arata/tutorialmod/recipe/GemPolishingRecipe.java
@@ -26,6 +26,11 @@ public class GemPolishingRecipe implements Recipe<SimpleContainer> {
     }
 
     @Override
+    public NonNullList<Ingredient> getIngredients() {
+        return inputItems;
+    }
+
+    @Override
     public boolean matches(SimpleContainer pContainer, Level pLevel) {
         if(pLevel.isClientSide()) {
             return false;
